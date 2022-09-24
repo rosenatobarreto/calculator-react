2527358f39212b9d0f3a9cd2f9e6ee8dae8ec425
Rosenato Barreto <rosenatoblima@gmail.com>
2022-09-24 01:19:41 -0300

    Projeto calculadora React

---
+++ src/App.js
@@ -0,0 +1,129 @@
+import Input from './components/Input';
+import Button from './components/Button';
+import { Container, Content, Row } from './styles';
+import { useState } from 'react'
+
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
+
+  const handleOnClear = () => {
+    setCurrentNumber('0');
+    setFirstNumber('0');
+    setOperation('');
+  }
+
+  const handleAddNumber = (num) => {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
+  }
+
+  const handleSumNumber = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('+');
+    } else {
+      const sum = Number(firstNumber) + Number(currentNumber); 
+      setCurrentNumber(String(sum));
+      setOperation('');
+      
+    }
+  }
+
+    const handleMinusNumber = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('-');
+    } else {
+      const calc = Number(firstNumber) - Number(currentNumber); 
+      setCurrentNumber(String(calc));
+      setOperation('');
+      
+    }
+  }
+
+  const handleMultNumber = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('*');
+    } else {
+      const mult = Number(firstNumber) * Number(currentNumber); 
+      setCurrentNumber(String(mult));
+      setOperation('');
+      
+    }
+  }
+
+  const handleDividerNumber = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('/');
+    } else {
+      const divider = Number(firstNumber) / Number(currentNumber); 
+      setCurrentNumber(String(divider));
+      setOperation('');
+      
+    }
+  }
+
+
+  const handleEquals = () => {
+    if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
+      switch(operation){
+        case'+':
+          handleSumNumber();
+        break;
+        case'-':
+          handleMinusNumber();
+        break;
+        case'*':
+          handleMultNumber();
+        break;
+        case'/':
+          handleDividerNumber();
+        break;
+        default:
+        break;
+      }
+    } 
+  }
+
+  return (
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber('7')}/>
+          <Button label="8" onClick={() => handleAddNumber('8')}/>
+          <Button label="9" onClick={() => handleAddNumber('9')}/>
+          <Button label="*" onClick={handleMultNumber}/>
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber('4')}/>
+          <Button label="5" onClick={() => handleAddNumber('5')}/>
+          <Button label="6" onClick={() => handleAddNumber('6')}/>
+          <Button label="-" onClick={handleMinusNumber}/>
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')}/>
+          <Button label="2" onClick={() => handleAddNumber('2')}/>
+          <Button label="3" onClick={() => handleAddNumber('3')}/>
+          <Button label="+" onClick={handleSumNumber}/>
+        </Row>
+        <Row>
+          <Button label="c" onClick={handleOnClear}/>
+          <Button label="0" onClick={() => handleAddNumber('0')}/>
+          <Button label="/" onClick={handleDividerNumber}/>
+          <Button label="=" onClick={handleEquals}/>
+        </Row>
+      </Content>
+            
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding: 0 10px;
+        font-size: 26px;
+        font-family: 'Verdana';
+        color: #FFFFFF;
+    }
+
+
+
+
+
+
+
+
+
+`
